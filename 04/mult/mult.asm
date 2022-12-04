636aad60869ad7699ab96b92546ebe075765a999
侯皓騰 <99935453+asd1389271903@users.noreply.github.com>
2022-12-04 17:05:51 +0800

    Add files via upload

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,27 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@0
+D=A
+@R2
+M=0//把D跟sum都=0
+
+(LOOP)
+@R0
+D=M
+@END
+D;JEQ//結束條件 R0=0
+@R1
+D=M
+@R2
+M=M+D
+@R0
+M=M-1//把R0當成加的次數
+
+
+@LOOP
+D;JGT//if R0>0 goto loop
+
+(END)
+@END
+D;JEQ//if R0=0 goto end
